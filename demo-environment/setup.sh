8894da7bf0bcb91ca37bcd2292660f5a10110233
Carlos López Bartolomé <carlos3lb@hotmail.com>
2024-02-26 15:21:11 +0100

    Included job to clone repositories as part of Gitea deployment

--- demo-environment/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# GitOps Configuration
-oc adm policy add-cluster-role-to-user cluster-admin  system:serviceaccount:openshift-gitops:openshift-gitops-argocd-application-controller -n openshift-gitops
-
-# Create environment using GitOps
-oc apply -f demo-environment/demo-environment.yaml
