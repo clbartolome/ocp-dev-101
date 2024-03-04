09492ec39bc4ebf5809f13376685333e8e17695c
Carlos López Bartolomé <carlos3lb@hotmail.com>
2024-03-04 11:45:39 +0100

    renamed gitops namespace

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # GitOps Configuration
-oc adm policy add-cluster-role-to-user cluster-admin  system:serviceaccount:openshift-gitops:openshift-gitops-argocd-application-controller -n openshift-gitops
+oc adm policy add-cluster-role-to-user cluster-admin  system:serviceaccount:openshift-gitops-operator:openshift-gitops-operator-argocd-application-controller -n openshift-gitops-operator
 
 # Create environment using GitOps
 oc apply -f demo-environment/demo-environment.yaml
 
 # Instructions
 ARGO_USER=admin
-ARGO_PASS=$(oc get secret openshift-gitops-cluster -n openshift-gitops -ojsonpath='{.data.admin\.password}' | base64 -d)
-ARGO_ROUTE=$(oc get route openshift-gitops-server -n openshift-gitops -o jsonpath='{.status.ingress[0].host}')
+ARGO_PASS=$(oc get secret openshift-gitops-operator-cluster -n openshift-gitops-operator -ojsonpath='{.data.admin\.password}' | base64 -d)
+ARGO_ROUTE=$(oc get route openshift-gitops-operator-server -n openshift-gitops-operator -o jsonpath='{.status.ingress[0].host}')
 
 echo "Login into ArgoCD using the following info:"
 echo ""
